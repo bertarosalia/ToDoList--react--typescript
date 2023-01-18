e683ca74bdd9ef7381b4f09c6a9b7cef74c89cc2
Berta Floran <bertafloran@hotmail.com>
2023-01-18 22:56:36 +0100

    add Tasks to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
 import "./App.css";
+import Tasks from "./components/Tasks/Tasks";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Tasks />
+    </div>
+  );
 }
 
 export default App;

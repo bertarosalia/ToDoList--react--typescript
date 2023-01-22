b6b2005c888b0a5146721d86196bab7c3e098682
Berta Floran <bertafloran@hotmail.com>
2023-01-22 23:01:03 +0100

    add handleChange to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,34 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import "./App.css";
-import { ToDoList } from "./components/ToDoList/ToDoList";
-
-// interface Task {
-//   name: string;
-//   priority: number;
-//   time: number;
-//   description: string;
-// }
-
-// const intialStateTask = [
-//   {
-//     name: "Crossfit",
-//     priority: 5,
-//     time: 1,
-//     description: "Do crossfit on monday from 10 to 11 am",
-//   },
-//   {
-//     name: "Eat",
-//     priority: 5,
-//     time: 3,
-//     description: "Eat at 9pm, 2pm, 6pm and 9pm",
-//   },
-// ];
-
-// // const [tasks, setTask] = useState<Array<Task>>([]);
-// // useEffect(() => {
-// //   setTask(intialStateTask);
-// // }, []);
 
 const App = (): JSX.Element => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todo, setTodo] = useState([]);
 
-  // const todoTaskRef = useRef();
-
-  // const handleTodoAdd = () => {
-  //   const task = todoTaskRef.current.value;
-  //   if (task === "") return;
-
-  //   setTodos((prevTodos) => {
-  //     return [...prevTodos, { id: uuidv4(), name, completed: false }];
-  //   });
-  //   todoTaskRef.current.value = null;
-  // };
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
 
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task..." />
-          <input type="number" placeholder="Deadline (in days)" />
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in days)"
+            name="deadline"
+          />
         </div>
         <button className="addTask">Add Task</button>
       </div>
